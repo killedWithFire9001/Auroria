af846efb352829b7c2ac4a443467aacec993d825
ShaderWave <thomas.graalzone@gmail.com>
2017-02-07 17:38:12 +0000

    Queue - fixed! (Finally)

--- commands/queue.js
+++ commands/queue.js
@@ -5,9 +5,6 @@ var main = require("../bot.js");
 var Discord = require("discord.js");
 
 exports.run = function(msg) {
-  msg.reply("Command is under maintenance. Stupid Josh broke it, let him (me) fix it.");
-  return;
-
   var bot = main.bot;
   var config = main.config;
   var cmd = config["prefix_" + msg.channel.guild.id];
@@ -31,14 +28,19 @@ exports.run = function(msg) {
 
    toSend = [];
 
-   if (typeof musQueue[msg.channel.guild.id] == undefined || typeof musQueue[msg.channel.guild.id][1] == undefined) {
-   	msg.reply("Queue is empty!");
+   if (musQueue[msg.channel.guild.id] == undefined || musQueue[msg.channel.guild.id][1] == undefined) {
+   	msg.channel.sendMessage("Queue is empty!");
    	return;
    }
 
+   if (musQueue[msg.guild.id][2] == undefined) {
+    msg.channel.sendMessage("Current song: **" + musQueue[msg.guild.id][1].title + "** requested by **" + musQueue[msg.guild.id][1].requester + "**.\n*(Queue is empty)*");
+    return;
+   }
+
    for (i = 0; i < musQueue[msg.guild.id].length; i++) {
-   	if (i != 0 || i != 1 || i != -1) {
-   		toSend.push(`#${i} | ${musQueue[msg.guild.id][i].title}. [Link](${musQueue[msg.guild.id][i].url}) | Requested by ${musQueue[msg.guild.id][i].requester}`);
+   	if (i !== 0 && i !== 1) {
+   		toSend.push(`#${i-1} | ${musQueue[msg.guild.id][i].title}. [Link](${musQueue[msg.guild.id][i].url}) | Requested by ${musQueue[msg.guild.id][i].requester}`);
    	}
    }
 
@@ -49,7 +51,7 @@ exports.run = function(msg) {
   					.setDescription(``)
   					.setFooter("", "")
   					.setImage('')
- 					.setThumbnail( "" )
+ 					  .setThumbnail( "" )
   					.setTimestamp( '' )
   					.setURL('')
   					.addField("-> Current", musQueue[msg.channel.guild.id].current)
