6be1622383db9e677092c3cbfc541b7700e76e9b
ShaderWave <thomas.graalzone@gmail.com>
2017-02-26 21:20:42 +1100

    Debugging clean ;-;

--- commands/clean.js
+++ commands/clean.js
@@ -4,22 +4,23 @@ exports.syntax = "clean"
 var main = require("../bot.js");
 var Discord = require("discord.js");
 
-exports.run = function(msg) {
+exports.run = function (msg) {
   var bot = main.bot;
 
   if (!msg.guild.member(msg.author).hasPermission("MANAGE_MESSAGES")) {
-  	msg.channel.sendMessage("You can not use this command: You lack the Manage Messages permission.");
-  	return;
+    msg.channel.sendMessage("You can not use this command: You lack the Manage Messages permission.");
+    return;
   }
 
   msg.channel.fetchMessages({
-            limit: 100
-        }).then(messages => {
-      let msgfilter = messages.filter(e => e.author.id == bot.user.id);
+    limit: 100
+  }).then(messages => {
+    let msgfilter = messages.filter(e => e.author.id == bot.user.id);
 
-      if (msgfilter !== null || msgfilter !== undefined) {
-      	msg.channel.bulkDelete(msgfilter)
-      		.then(msg.channel.sendMessage("Deleted " + msgfilter.size + " messages sent from the Bot."));
-      }
-	});
+    if (msgfilter !== null || msgfilter !== undefined) {
+      msg.channel.bulkDelete(msgfilter)
+        .then(msg.channel.sendMessage("Deleted " + msgfilter.size + " messages sent from the Bot."))
+        .catch(err => msg.channel.sendMessage('Caught Error: `' + err + '`'));
+    }
+  });
 }
